2cec83520652468df573c9549c6d7be1ff8b29c8
codo-spec <exform.arc@gmail.com>
2024-11-14 02:53:35 +0530

    update

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("1st middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+   res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button</form>')
+
 })
 
-app.use((req,res,next)=>{
-    console.log("2nd middleware");
-    res.send('<h1>Hello</h1>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+   res.send('<h1>Hello</h1>');
 })
 app.listen(3000);
