175d3fecad3dbbacf33c3de7870881b752630282
codo-spec <exform.arc@gmail.com>
2024-11-14 01:35:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log("1st middleware");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("2nd middleware");
+    res.send('<h1>Hello</h1>')
+})
+app.listen(3000);
